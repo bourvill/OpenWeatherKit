d6863a70f1cec7415a1acdb61cc3f0deccffce14
maxime marinel <maximemarinel@MacBook-Pro-de-maxime.local>
2017-11-03 13:26:45 +0100

    Reboot project

---
+++ Sources/OpenWeatherKit.swift
@@ -0,0 +1,9 @@
+//
+//  OpenWeatherKit.swift
+//  OpenWeatherKit
+//
+//  Created by Marinel Maxime on 03/11/2017.
+//  Copyright © 2017 OpenWeatherKit. All rights reserved.
+//
+
+import Foundation
